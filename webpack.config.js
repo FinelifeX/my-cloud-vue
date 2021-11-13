71a8532bffcbbc7b219a5b391f26079f51fa87e0
Bulat Murtazin <bulatf.murtazin@gmail.com>
2021-11-14 00:45:33 +0300

    = reformat webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -7,15 +7,15 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   mode: process.env.NODE_ENV,
-  entry: "./src/main.js",
+  entry: './src/main.js',
   output: {
-    filename: "bundle.min.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.min.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  devtool: isProduction ? "hidden-source-map" : "inline-source-map",
+  devtool: isProduction ? 'hidden-source-map' : 'inline-source-map',
   devServer: {
-    static: "./dist",
+    static: './dist',
     hot: true,
   },
   module: {
@@ -26,30 +26,25 @@ module.exports = {
       },
       {
         test: /.js$/i,
-        loader: "babel-loader",
-        exclude: (file) => (
-          /node_modules/.test(file)
-          && !/\.vue\.js/.test(file)
-        ),
+        loader: 'babel-loader',
+        exclude: (file) => /node_modules/.test(file) && !/\.vue\.js/.test(file),
       },
       {
         test: /.s?css$/i,
         use: [
-          isProduction
-            ? MiniCssExtractPlugin.loader
-            : "vue-style-loader",
-          "css-loader",
-          "sass-loader",
-        ]
-      }
-    ]
+          isProduction ? MiniCssExtractPlugin.loader : 'vue-style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
   },
   plugins: [
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-      filename: path.resolve(__dirname, "dist", "index.html"),
-      inject: "body"
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      filename: path.resolve(__dirname, 'dist', 'index.html'),
+      inject: 'body',
     }),
-  ]
-}
+  ],
+};
