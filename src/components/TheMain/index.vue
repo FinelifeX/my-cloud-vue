36a8ce72e977345e96bda383aa769a22193e10ef
Bulat Murtazin <bulatf.murtazin@gmail.com>
2021-11-23 18:57:25 +0300

    = main layout base

--- src/components/TheMain/index.vue
+++ src/components/TheMain/index.vue
@@ -1,11 +1,26 @@
 <template>
-  <main>Main content</main>
+  <main class="main">
+    <the-main-header />
+    <the-main-content />
+  </main>
 </template>
 
 <script>
+  import TheMainHeader from '@components/TheMain/TheMainHeader';
+  import TheMainContent from '@components/TheMain/TheMainContent';
+
   export default {
     name: 'TheMain',
+    components: { TheMainContent, TheMainHeader },
   };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+  .main {
+    @apply flex-1;
+    @apply m-5;
+    @apply p-5;
+    @apply rounded-3xl;
+    @apply bg-selago;
+  }
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.html', './src/**/*.vue'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        selago: '#F4F6FD',
+        spun_pearl: '#ABA8B2',
+      },
+    },
   },
   variants: {
     extend: {},
