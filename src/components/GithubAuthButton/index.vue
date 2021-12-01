16caec07d2b12868b5742a08acf50c8f098c176d
Bulat Murtazin <bulatf.murtazin@gmail.com>
2021-12-01 16:19:33 +0300

    = github auth button wip

---
+++ src/components/GithubAuthButton/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="github-auth-button">
+    <img class="github-auth-button__icon" :src="icon" alt="GitHub" />
+    Sign in with GitHub
+  </button>
+</template>
+
+<script>
+  import GithubImg from '@assets/images/github_dark.png';
+
+  export default {
+    name: 'GithubAuthButton',
+    data() {
+      return {
+        icon: GithubImg,
+      };
+    },
+  };
+</script>
+
+<style scoped lang="scss">
+  .github-auth-button {
+    @apply flex flex-row items-center;
+    @apply text-lg;
+    @apply px-4 py-2;
+    @apply rounded-md;
+    @apply bg-white;
+    @apply shadow-md;
+    @apply transform duration-100 hover:scale-105;
+  }
+
+  .github-auth-button__icon {
+    @apply h-8;
+    @apply mr-4;
+  }
+</style>
