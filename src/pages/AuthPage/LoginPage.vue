c7a443624ea59bf825371591974f7ba2e2955c09
Bulat Murtazin <bulatf.murtazin@gmail.com>
2021-12-13 01:08:57 +0300

    = wip

--- src/pages/AuthPage/LoginPage.vue
+++ src/pages/AuthPage/LoginPage.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
-    <div>Login Page</div>
-    <div>
-      <github-auth-button />
-    </div>
+  <div class="login__wrapper">
+    <form class="login__form">
+      <h1 class="login__form__title">Log in</h1>
+      <input class="input" placeholder="E-mail" />
+      <input class="input" placeholder="Password" />
+      <div>
+        <github-auth-button />
+      </div>
+    </form>
   </div>
 </template>
 
@@ -16,4 +20,31 @@
   };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+  @import '~@assets/styles/_fonts.scss';
+
+  .login__wrapper {
+    @apply flex flex-row justify-center items-center;
+  }
+
+  .login__form {
+    @apply w-full;
+
+    @media (min-width: 321px) {
+      max-width: 320px;
+    }
+  }
+
+  .login__form__title {
+    @apply text-3xl;
+    @apply text-center;
+
+    @include FontOutfit;
+  }
+
+  .input {
+    @apply border border-spun_pearl;
+    @apply rounded-md;
+    @apply px-2 py-1;
+  }
+</style>
